612df240b004511b979441fe7f5d65571eb8b514
Phil Scalo <phillip.scalo@redcross.org>
2023-12-03 01:44:29 -0500

    Exercise 2.16

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,26 @@
 # pcost.py
 #
-# Exercise 1.27
+# Exercise 1.27, 2.15
+import csv
+
+
+def calc_cost_basis(filename: str) -> float:
+    with open(filename) as fp:
+        rows = csv.reader(fp)
+        headers = next(rows)
+        total_cost = 0.0
+        for rowno, row in enumerate(rows, start=1):
+            record = dict(zip(headers, row))
+            try:
+                nshares = int(record['shares'])
+                price = float(record['price'])
+                total_cost += nshares * price
+            except ValueError:
+                print(f'Row {rowno}: Bad row: {row}')
+                continue
+
+    return total_cost
+
+
+if __name__ == '__main__':
+    print(f"Total cost {calc_cost_basis('Data/portfolio.csv'):.2f}")
--- Work/report.py
+++ Work/report.py
@@ -1,19 +1,30 @@
 # report.py
 #
-# Exercise 2.4
+# Exercise 2.11
 
 import csv
 
 
-def read_portfolio(filename: str) -> list[dict]:
+def read_portfolio(filename: str) -> list[dict[str, int, float]]:
     """
-    Reads a given portfolio file into a list of tuples
+    Reads a given portfolio file into a list of dictionaries
     """
     portfolio = []
 
     with open(filename, "rt") as f:
-        rows = csv.DictReader(f)
-        for row in rows:
+        rows = csv.reader(f)
+        headers = ['name', 'shares', 'price']
+        next(rows)
+        for rowno, row in enumerate(rows, start=1):
+            record = dict(zip(headers, row))
+            try:
+                name = record['name']
+                shares = int(record['shares'])
+                price = float(record['price'])
+                row = name, shares, price
+            except ValueError:
+                print(f'Row {rowno}: Bad row: {row}')
+            row = dict(zip(headers, row))
             portfolio.append(row)
 
     return portfolio
@@ -28,13 +39,13 @@ def read_prices(filename: str) -> dict:
         rows = csv.reader(f)
         for row in rows:
             try:
-                price_dict.update({row[0]: row[1]})
+                price_dict.update({row[0]: float(row[1])})
             except IndexError as e:
                 print(f"Empty line in file input: {e}")
     return price_dict
 
 
-def calc_gains(portfolio, market_report):
+def calc_gains(portfolio: str, market_report: str) -> float:
     """
     reads a portfolio, compares to current price file, and returns calculated
     unrealized gains of the portfolio
@@ -46,7 +57,7 @@ def calc_gains(portfolio, market_report):
     portfolio_basis = read_portfolio(portfolio)
     portfolio_current = read_prices(market_report)
     for stock in portfolio_basis:
-        ticker = stock['name']
+        ticker = stock["name"]
         num_shares = int(stock["shares"])
         basis = num_shares * float(stock["price"])
         total_cost_basis += basis
@@ -62,5 +73,38 @@ def calc_gains(portfolio, market_report):
     return unrealized_gains
 
 
+def make_report(portfolio: list, prices: dict[str, float]) -> list[tuple]:
+    """
+    reads stock prices and returns a table
+    """
+    table_rows = []
+    for stock in portfolio:
+        try:
+            ticker = stock["name"]
+        except ValueError as e:
+            print(f'{stock} has no ticker: {e}')
+        basis = stock["price"]
+        num_shares = stock["shares"]
+        price = prices[ticker]
+        change = price - basis
+
+        table_row = (ticker, num_shares, price, change)
+        table_rows.append(table_row)
+    return table_rows
+
+
+def display_report(data: list):
+    headers = ('Name', 'Shares', 'Price', 'Change')
+    name, shares, price, change = headers
+    sep = '-'*10
+
+    print(f'{name:>10s} {shares:>10s} {price:>10s} {change:>10s}')
+    print(f'{sep:>10s} {sep:>10s} {sep:>10s} {sep:>10s} ')
+    for row in data:
+        print('%10s %10d %10.2f %10.2f' % row)
+
+
 if __name__ == "__main__":
-    print(calc_gains("Data/portfolio.csv", "Data/prices.csv"))
+    portfolio = read_portfolio("Data/portfolio.csv")
+    prices = read_prices("Data/prices.csv")
+    print(make_report(portfolio, prices))
