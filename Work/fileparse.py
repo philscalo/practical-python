9196955d730aa67b1cd049b4f03794b8ed691bc3
Phil Scalo <phillip.scalo@redcross.org>
2023-12-06 15:51:41 -0500

    Chap 3, incomplete

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,35 @@
 # fileparse.py
 #
 # Exercise 3.3
+import csv
+
+
+def parse_csv(filename, select=None, types=None):
+    """
+    Parse a CSV file into a list of records
+    """
+    with open(filename) as f:
+        rows = csv.reader(f)
+
+        # Read the file headers
+        headers = next(rows)
+
+        if select:
+            indices = [headers.index(colname) for colname in select]
+            headers = select
+        else:
+            indices = []
+
+        records = []
+        for row in rows:
+            if not row:
+                continue
+            if indices:
+                row = [row[index] for index in indices]
+            if types:
+                row = [func(val) for func, val in zip(types, row)]
+
+            record = dict(zip(headers, row))
+            records.append(record)
+
+    return records
