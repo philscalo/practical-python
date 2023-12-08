5bb2c02480d7e751ff6c5acb0726ecca15ac4305
Phil Scalo <phillip.scalo@redcross.org>
2023-12-08 11:31:42 -0500

    Through ex 3.6

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -4,15 +4,15 @@
 import csv
 
 
-def parse_csv(filename, select=None, types=None):
+def parse_csv(filename, select=None, types=None, has_headers=True):
     """
     Parse a CSV file into a list of records
     """
     with open(filename) as f:
         rows = csv.reader(f)
 
-        # Read the file headers
-        headers = next(rows)
+        if has_headers:
+            headers = next(rows)
 
         if select:
             indices = [headers.index(colname) for colname in select]
@@ -29,7 +29,10 @@ def parse_csv(filename, select=None, types=None):
             if types:
                 row = [func(val) for func, val in zip(types, row)]
 
-            record = dict(zip(headers, row))
+            if has_headers:
+                record = dict(zip(headers, row))
+            else:
+                record = tuple(row)
             records.append(record)
 
     return records
