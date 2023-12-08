fe14ffc0df82d5ee08f02228e5b6beabb510b891
Phil Scalo <phillip.scalo@redcross.org>
2023-12-08 13:24:47 -0500

    Through ex 3.10

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,14 +1,23 @@
 # fileparse.py
 #
-# Exercise 3.3
+# Exercise 3.10
 import csv
 
 
-def parse_csv(filename, select=None, types=None, has_headers=True, delimit_char=','):
+def parse_csv(
+        filename,
+        select=None,
+        types=None,
+        has_headers=True,
+        delimit_char=',',
+        silence_errors=False
+    ):
     """
     Parse a CSV file into a list of records
     """
     with open(filename) as f:
+        if select and has_headers is False:
+            raise RuntimeError("select argument requires column headers")
         if delimit_char and delimit_char != ',':
             rows = csv.reader(f, delimiter=delimit_char)
         else:
@@ -24,13 +33,20 @@ def parse_csv(filename, select=None, types=None, has_headers=True, delimit_char=
             indices = []
 
         records = []
-        for row in rows:
+        for rowno, row in enumerate(rows, start=1):
             if not row:
                 continue
             if indices:
                 row = [row[index] for index in indices]
             if types:
-                row = [func(val) for func, val in zip(types, row)]
+                try:
+                    row = [func(val) for func, val in zip(types, row)]
+                except ValueError as e:
+                    if silence_errors != True:
+                        print(f"Row {rowno}: Couldn't convert {row}")
+                        print(f"Row {rowno}: {e}")
+                    else:
+                        continue
 
             if has_headers:
                 record = dict(zip(headers, row))
